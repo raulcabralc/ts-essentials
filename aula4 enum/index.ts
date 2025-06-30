2072a10a45c21957b87f25ae1e1274b187c7363f
Raul Cabral <raulcabralc@gmail.com>
2025-06-30 17:36:20 -0300

    Enum

---
+++ aula4 enum/index.ts
@@ -0,0 +1,15 @@
+// const admin = 0;
+// const backend = 1;
+// const frontend = 2;
+
+enum Role {
+  admin = 0,
+  backend = 1,
+  frontend = 2,
+}
+
+const user = {
+  name: "Raul",
+  age: 18,
+  role: Role.backend,
+};
