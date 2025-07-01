e9fbe8de10b5b0e95f877af1c9e5c2346c47c9c2
Raul Cabral <raulcabralc@gmail.com>
2025-07-01 13:59:34 -0300

    Object

---
+++ aula7 objects/index.ts
@@ -0,0 +1,21 @@
+const user: {
+  name: string;
+  age: number;
+  surname: string;
+} = {
+  name: "Raul",
+  age: 18,
+  surname: "Cabral",
+};
+
+/* Sintaxe:
+
+const object: {
+  Key: tipo da key;
+  Key2: tipo da key2;
+} = {
+  Key: valor,
+  Key2: valor2
+}
+
+*/
