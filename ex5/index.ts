d24c541f4e78da32ababd6e2268468677707ef6f
Raul Cabral <raulcabralc@gmail.com>
2025-07-01 16:43:48 -0300

    Exercício 5

---
+++ ex5/index.ts
@@ -0,0 +1,10 @@
+const sequence: number[] = Array.from(Array(10).keys());
+const animals: string[] = ["pangolin", "aardvark", "echidna", "binturong"];
+const stringsAndNumbers: (number | string)[] = [1, "one", 2, "two", 3, "three"];
+const allMyArrays: (number | string)[][] = [
+  sequence,
+  animals,
+  stringsAndNumbers,
+];
+
+console.log(allMyArrays);
