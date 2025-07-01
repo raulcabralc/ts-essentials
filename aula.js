579c1fa15bfb2a1dff4ec2c6a09539c3d8e8fbff
Raul Cabral <raulcabralc@gmail.com>
2025-07-01 18:05:34 -0300

    Error Handling para os Criadores

--- aula.js
+++ aula.js
@@ -13,12 +13,14 @@ const cores = {
   green: "\x1b[32m",
   yellow: "\x1b[33m",
   blue: "\x1b[34m",
+  red: "\x1b[31m",
+  white: "\x1b[37m",
 };
 
 console.clear();
 
 function createAula(aulaNum, aulaNome) {
-  const aula = `aula${aulaNum} ${aulaNome}`;
+  const aula = aulaNome ? `aula${aulaNum} ${aulaNome}` : `aula${aulaNum}`;
   mkdirSync(aula);
 
   writeFileSync(`./${aula}/index.ts`, "");
@@ -32,15 +34,64 @@ function startApp() {
     rl.question(
       `${cores.yellow + cores.bright}Tópico:${cores.reset} `,
       (name) => {
-        createAula(num, name.toLowerCase());
-        console.clear();
-        console.log(
-          `${cores.reset}\n${cores.green + cores.bright}✓${cores.reset} Aula ${
-            cores.green + cores.bright
-          }${num}${cores.reset} criada com o nome ${
-            cores.green + cores.bright
-          }${name}${cores.reset}!\n`
-        );
+        let errors = [];
+        try {
+          console.clear();
+
+          if (!num) {
+            errors.push(
+              `É necessário ${cores.yellow + cores.bright}adicionar um número${
+                cores.reset
+              } para a ${cores.yellow + cores.bright}aula${cores.reset}.`
+            );
+            throw new Error();
+          }
+
+          try {
+            const numTry = Number(num);
+            if (isNaN(numTry)) throw new Error();
+          } catch (e) {
+            errors.push(
+              `O ${cores.yellow + cores.bright}número${
+                cores.reset
+              } da aula deve ser um ${cores.yellow + cores.bright}número${
+                cores.reset
+              }.`
+            );
+            throw new Error();
+          }
+
+          createAula(num, name.toLowerCase());
+
+          console.log(
+            `${cores.reset}\n${cores.green + cores.bright}✔${
+              cores.reset
+            } Aula ${cores.green + cores.bright}${num}${cores.reset} criada${
+              name
+                ? ` com o nome ${cores.green + cores.bright}${name}${
+                    cores.reset
+                  }`
+                : ""
+            }!\n`
+          );
+        } catch (e) {
+          console.log(
+            name
+              ? `\n${cores.red + cores.bright}✘${cores.reset} Falha ao criar ${
+                  cores.red + cores.bright
+                }aula${num} ${name}${cores.reset}!\n`
+              : `\n${cores.red + cores.bright}✘${cores.reset} Falha ao criar ${
+                  cores.red + cores.bright
+                }aula${num}${cores.reset}!\n`
+          );
+
+          errors.forEach((error) => {
+            console.log(
+              `${cores.yellow + cores.bright}⚠ ${cores.reset} ${error}\n`
+            );
+          });
+        }
+
         rl.close();
       }
     );
--- exercicio.js
+++ exercicio.js
@@ -13,6 +13,8 @@ const cores = {
   green: "\x1b[32m",
   yellow: "\x1b[33m",
   blue: "\x1b[34m",
+  red: "\x1b[31m",
+  white: "\x1b[37m",
 };
 
 console.clear();
@@ -31,13 +33,57 @@ function startApp() {
       cores.reset
   );
   rl.question(`${cores.yellow + cores.bright}Número:${cores.reset} `, (num) => {
-    createExercício(num);
-    console.clear();
-    console.log(
-      `${cores.reset}\n${cores.green + cores.bright}✓${cores.reset} Exercício ${
-        cores.green + cores.bright
-      }${num}${cores.reset} criado!\n`
-    );
+    let errors = [];
+
+    try {
+      if (!num) {
+        errors.push(
+          `É necessário ${cores.yellow + cores.bright}adicionar um número${
+            cores.reset
+          } para o ${cores.yellow + cores.bright}exercício${cores.reset}.`
+        );
+        throw new Error();
+      }
+
+      try {
+        const numTry = Number(num);
+        if (isNaN(numTry)) throw new Error();
+      } catch (e) {
+        errors.push(
+          `O ${cores.yellow + cores.bright}número${
+            cores.reset
+          } do exercício deve ser um ${cores.yellow + cores.bright}número${
+            cores.reset
+          }.`
+        );
+        throw new Error();
+      }
+
+      createExercício(num);
+
+      console.clear();
+
+      console.log(
+        `${cores.reset}\n${cores.green + cores.bright}✔${
+          cores.reset
+        } Exercício ${cores.green + cores.bright}${num}${cores.reset} criado!\n`
+      );
+    } catch (e) {
+      console.clear();
+
+      console.log(
+        `\n${cores.red + cores.bright}✘${cores.reset} Falha ao criar ${
+          cores.red + cores.bright
+        }exercício${num}${cores.reset}!\n`
+      );
+
+      errors.forEach((error) => {
+        console.log(
+          `${cores.yellow + cores.bright}⚠ ${cores.reset} ${error}\n`
+        );
+      });
+    }
+
     rl.close();
   });
 }
